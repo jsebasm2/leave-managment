2921d2bd8a9f431286178af53d45a2ad4b8aec58
t_moraj <juan.mora@autodesk.com>
2020-06-30 19:35:04 -0500

    Add project files.

---
+++ leave-managment/Implementations/LeaveHistoryImplementation.cs
@@ -0,0 +1,54 @@
+﻿using leave_managment.Data;
+using leave_managment.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_managment.Implementations
+{
+    public class LeaveHistoryImplementation : ILeaveHistoryInterface
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LeaveHistoryImplementation(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool Create(LeaveHistory entity)
+        {
+            _db.LeaveHistories.Add(entity);
+            return Save();
+        }
+
+        public bool Delete(LeaveHistory entity)
+        {
+            _db.LeaveHistories.Remove(entity);
+            return Save();
+        }
+
+        public ICollection<LeaveHistory> FindAll()
+        {
+            var leaveHistories = _db.LeaveHistories.ToList();
+            return leaveHistories;
+        }
+
+        public LeaveHistory FindById(int id)
+        {
+            var leaveHistories = _db.LeaveHistories.Find(id);
+            return leaveHistories;
+        }
+
+        public bool Save()
+        {
+            return _db.SaveChanges() > 0;
+        }
+
+        public bool Update(LeaveHistory entity)
+        {
+            _db.LeaveHistories.Update(entity);
+            return Save();
+        }
+    }
+}
